de51485c90d37ad7c385b0fcce42ddd9063c24e7
ismail <votre_email@example.com>
2025-04-18 13:24:25 +0100

    update1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>Mon Site</title>
+    <title>event-reserve</title>
 </head>
 <body>
     <!-- Header -->
