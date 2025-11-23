bef5e445f12030f3f2ada8129b7223a7078db256
ismailessaouabi <ismailessaouabi63@gmail.com>
2025-11-23 10:38:20 +0100

    teste deploy railway!

--- resources/views/pages/layouts.blade.php
+++ resources/views/pages/layouts.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.3/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Event-reserve | Guichet en ligne</title>
 </head>
 <body class="bg-[#051529] text-white">
